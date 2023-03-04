447b08694c7911f79b149ccbc464dcd0e7907e7a
haoruikun <haoruikun@icloud.com>
2023-03-03 23:58:43 -0700

    giveoutpass UI in progress

--- pages/giveoutpass/[oid].tsx
+++ pages/giveoutpass/[oid].tsx
@@ -30,13 +30,47 @@ import {
 } from '../../lib/constants';
 import { toast } from "react-toastify";
 import namehash from "@ensdomains/eth-ens-namehash";
-import { ethers } from 'ethers';
-import { Typography } from "@ensdomains/thorin";
-import CopyShare from "../../utils/CopyShare";
-import { CheckCircleSVG } from "@ensdomains/thorin";
+import { Tabs } from 'antd';
+import { LogoutOutlined, CloudUploadOutlined } from '@ant-design/icons';
 import Container from '../../components/Container';
 
-export default function Home() {
+export default function GiveOutPass() {
+    return (
+        <div className={styles.container} style={{ overflow: "hidden" }
+        }>
+            <Head>
+                <title>M3MBER </title>
+                <meta name="description" content="M3MBER" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Container>
+                <Heading style={{ fontSize: "50px", marginBottom: 30 }}>Give out name</Heading>
+                <Card style={{ boxShadow: "0 8px 20px rgba(0,0,0,0.12)", padding: "20px 50px 50px 50px" }}>
+                    <Tabs
+                        defaultActiveKey="2"
+                        items={[LogoutOutlined, CloudUploadOutlined].map((Icon, i) => {
+                            const id = String(i + 1);
+                            return {
+                                label: (
+                                    <span>
+                                        <Icon />
+                                        {i === 0 ? "Manual give out" : "Import CSV"}
+                                    </span>
+                                ),
+                                key: id,
+                                children: i === 0 ? <ManualGive /> : <ImportCSV />,
+                            };
+                        })}
+                    />
+                </Card>
+
+            </Container>
+        </div>
+    )
+}
+
+
+function ManualGive() {
     const [oid, setOid] = useState("");
     const router = useRouter();
     useEffect(() => {
@@ -46,23 +80,19 @@ export default function Home() {
     })
 
     const [submitLoading, setSubmitLoading] = useState(false);
-    const [stopLoading, setStopLoading] = useState(false);
-    const [rule, setRule] = useState("");
     const [fee, setFee] = useState("0.05");
-    const [description, setDescription] = useState("")
-    const [mintSuccessAndShare, setMintSuccessAndShare] = useState(false);
     // const { address } = useAccount();
     const [subname, setSubname] = useState("");
     const [receiveraddress, setReceiverAddress] = useState("");
     const copyToClipBoard = async (text: string) => {
         try {
-          await navigator.clipboard.writeText(text)
-          toast.success('Copied to clipboard')
+            await navigator.clipboard.writeText(text)
+            toast.success('Copied to clipboard')
         } catch (err) {
-          console.error('Failed to copy text: ', err)
-          toast.error('Failed to copy to clipboard')
+            console.error('Failed to copy text: ', err)
+            toast.error('Failed to copy to clipboard')
         }
-      }
+    }
     //   const setM3mbershipDescriptionConfig = usePrepareContractWrite({
     //     address: ensResolverGoerli,
     //     abi: ensResolverAbiGoerli,
@@ -92,39 +122,23 @@ export default function Home() {
             0, // ttl
             0, // fuses
             0, // expiry
-            
+
         ],
         overrides: {
             gasLimit: '300000',
             value: 0,
         },
-      })
+    })
 
     const giveOutName = useContractWrite(giveOutNameConfig)
 
 
     const handleGive = () => {
-        if(giveOutName.isSuccess){
+        if (giveOutName.isSuccess) {
             copyToClipBoard(router.pathname + "/my/plans");
-        } else{
+        } else {
             giveOutName.write();
         }
-    }
-
-    const handleResume = () => {
-        //TODO: resume minting
-        setSubmitLoading(true);
-        setTimeout(() => {
-            setSubmitLoading(false);
-        }, 1000);
-    }
-
-    const handleStop = () => {
-        //TODO: stop minting
-        setStopLoading(true);
-        setTimeout(() => {
-            setStopLoading(false);
-        }, 1000);
     }
 
     const handleNameChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -140,53 +154,35 @@ export default function Home() {
     }
 
     return (
-        <div className={styles.container} style={{ overflow: "hidden" }
-        }>
-            <Head>
-                <title>M3MBER </title>
-                <meta name="description" content="M3MBER" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Container>
-                <Heading style={{ fontSize: "50px", marginBottom: 30 }}>Give out name</Heading>
-                <Card style={{ boxShadow: "0 8px 20px rgba(0,0,0,0.12)", padding: 50 }}>
-                    <div style={{ display: "flex", justifyContent: "space-between", width: "100%", marginBottom: "10px" }}>
-                    <div style={{ flex: "1 1 auto" }}>
-                            <p style={{ marginBottom: "10px", color: "#9B9BA5" }}>Subname name</p>
-                            <Input value={subname} onChange={handleNameChange} placeholder=""
-                                style={{ height: 50 }} />
-                        </div>
-                        <div style={{ flex: "1 1 auto" }}>
-                            <p style={{ marginBottom: "10px", color: "#9B9BA5" }}>Recipient Address</p>
-                            <Input value={receiveraddress} onChange={handleReceiverAddress} placeholder=""
-                                style={{ height: 50 }} />
-                        </div>
-                        <div style={{ flex: "1 1 auto", marginLeft: "10px" }}>
-                            <p style={{ marginBottom: "10px", color: "#9B9BA5" }}>Fee</p>
-                            <InputNumber
-                                size="large"
-                                stringMode
-                                className="fee-input"
-                                prefix={<img src="/eth.png" alt="etherem" style={{ height: 14, width: "100%" }} />}
-                                value={fee}
-                                onChange={handleFeeChange}
-                                min="0" defaultValue="0.05" step="0.01"
-                                style={{ height: 50 }} />
-                        </div>
-                        <div style={{ flex: "0 1 200px", marginLeft: "10px" }}>
-                            <p style={{ marginBottom: "10px", color: "#9B9BA5" }}>Period</p>
-                            <Input disabled value={"Monthly"}
-                                style={{ height: 50 }} />
-                        </div>
-                    </div>
-                    <Button key="submit" loading={submitLoading} onClick={handleGive}
-                        style={{ width: "200px", marginTop: 20 }}>
-                        { giveOutName.isSuccess? "Copy Invite" : "Give"}
-                    </Button>
+        <>
+            <div style={{ display: "flex", justifyContent: "space-between", width: "100%", marginBottom: "10px" }}>
+                <div style={{ flex: "1 1 auto" }}>
+                    <p style={{ marginBottom: "10px", color: "#9B9BA5" }}>Subname name</p>
+                    <Input value={subname} onChange={handleNameChange} placeholder=""
+                        style={{ height: 50 }} />
+                </div>
+                <div style={{ flex: "0 1 200px", marginLeft: 25 }}>
+                    <p style={{ marginBottom: "10px", color: "#9B9BA5" }}>Plans</p>
+                    <Input disabled value={"Monthly"}
+                        style={{ height: 50 }} />
+                </div>
+            </div>
+            <div style={{ display: "flex", justifyContent: "space-between", width: "100%", marginBottom: "10px" }}>
+                <div style={{ flex: "1 1 auto" }}>
+                    <p style={{ marginBottom: "10px", color: "#9B9BA5" }}>Recipient Address</p>
+                    <Input value={receiveraddress} onChange={handleReceiverAddress} placeholder=""
+                        style={{ height: 50 }} />
+                </div>
+            </div>
 
-                </Card>
+            <Button key="submit" loading={submitLoading} onClick={handleGive}
+                style={{ width: "200px", marginTop: 20 }}>
+                {giveOutName.isSuccess ? "Copy Invite" : "Give"}
+            </Button>
+        </>
+    );
+}
 
-            </Container>
-        </div>
-    )
+function ImportCSV() {
+    return <></>;
 }
