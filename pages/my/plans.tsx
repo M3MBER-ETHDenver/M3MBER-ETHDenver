c4175b438e9ed1401a2747fdfdcd7420a5ffdf31
haoruikun <haoruikun@icloud.com>
2023-03-03 23:16:44 -0700

    admin some clean up

--- pages/my/plans.tsx
+++ pages/my/plans.tsx
@@ -1,5 +1,20 @@
+import Container from "../../components/Container";
+import { Heading } from "@ensdomains/thorin";
+
+//data for table
+export interface DataType {
+    key: React.Key;
+    avatar: string;
+    name: [string, boolean];
+    domain: string;
+    address: string;
+    index: string;
+}
+
 export default function MyPlans({ Component, pageProps }) {
-
-
-    return <h1 style={{marginTop:"100px"}}>My Plans</h1>
+    return (
+        <Container>
+            <Heading style={{ fontSize: "50px", marginBottom: 30 }}>My Membership</Heading>
+        </Container>
+    );
 }
