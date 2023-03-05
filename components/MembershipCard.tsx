c8aaf91b7da7fd62f7ca0515ad827e48c9628c1f
haoruikun <haoruikun@icloud.com>
2023-03-05 00:40:22 -0700

    memershipcard loading

--- components/MembershipCard.tsx
+++ components/MembershipCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { Card } from "antd";
 
@@ -6,6 +6,13 @@ export default function MembershipCard(props) {
     const { text, data } = props;
     //add loading functionality later
     const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        setLoading(true);
+        if (props.data) {
+            setLoading(false);
+        }
+    }, [data])
+
     if (loading) {
         return (
             <Card style={{
